96f4acfb146b3647550f1348ae0a09cf9fd0e864
nikaoz <bwd430@alumni.ku.dk>
2021-07-26 18:07:20 +0200

    First commit
    Edit code. first commit.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,17 @@
 /**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
+ * @author Vinit Shahdeo <vinitshahdeo@gmail.com> Edit by Niklas
  */
-(function ($) {
+ (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
 
-/**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
- */
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
-    console.log('Shaadi me zaroor aana');
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
+var countDownDate = new Date("Aug 21, 2021 13:50:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -57,7 +42,7 @@ var x = setInterval(function() {
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
     , 'border: 4px solid #3E0E02'
-    , 'color: white'
+    , 'color: black'
     , 'display: block'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
     , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
@@ -68,7 +53,7 @@ var styles = [
 ].join(';');
 
 var styles1 = [
-    'color: #FF6C37'
+    'color: yellow'
     , 'display: block'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
     , 'line-height: 40px'
@@ -77,7 +62,7 @@ var styles1 = [
 ].join(';');
 
 var styles2 = [
-    'color: teal'
+    'color: yellow'
     , 'display: block'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
     , 'line-height: 40px'
@@ -85,11 +70,3 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
-
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
-
-console.log(
-    `%cShaadi me zaroor aana!\n\n`,
-    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-)
